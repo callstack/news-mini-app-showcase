7ceae6908c5760f9c71257ae6a8db21ee6bac331
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2025-03-20 12:08:42 +0100

    fix: shared deps

--- sharedDeps.js
+++ sharedDeps.js
@@ -1,20 +1,12 @@
-/**
- * Collect shared dependencies from the SDK and expose them
- * for the ModuleFederationPluginV1.
- *
- * @param {{ eager: boolean }} options Options for the shared dependencies. Use eager: false if using in a mini-app.
- * @returns Shared dependencies object.
- */
-const getSharedDependencies = ({ eager = true }) => {
-    const dependencies = require('./dependencies.json');
-  
-    const shared = Object.entries(dependencies)
-      .filter(([dep, props]) => props.shared !== false)
-      .map(([dep, { version }]) => {
-        return [dep, { singleton: true, eager, requiredVersion: version }];
-      });
-    return Object.fromEntries(shared);
-  };
-  
-  module.exports = getSharedDependencies;
-  
+const getSharedDependencies = ({eager = true}) => {
+  const {dependencies} = require('./package.json');
+
+  const shared = Object.entries(dependencies)
+    .filter(([dep]) => dep !== '@module-federation/enhanced')
+    .map(([dep, version]) => {
+      return [dep, {singleton: true, eager, requiredVersion: version}];
+    });
+  return Object.fromEntries(shared);
+};
+
+module.exports = getSharedDependencies;
