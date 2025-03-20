96daa77f6a7178e90e10e91a2d39a4f2fe7509e7
adamTrz <trzcinski.adam@gmail.com>
2025-03-20 11:36:28 +0100

    chore: upgrade repack and other deps

---
+++ rspack.config.mjs
@@ -0,0 +1,62 @@
+import path from 'node:path';
+import {fileURLToPath} from 'node:url';
+import * as Repack from '@callstack/repack';
+import rspack from '@rspack/core';
+import getSharedDependencies from './sharedDeps.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const STANDALONE = Boolean(process.env.STANDALONE);
+
+/**
+ * Rspack configuration enhanced with Re.Pack defaults for React Native.
+ *
+ * Learn about Rspack configuration: https://rspack.dev/config/
+ * Learn about Re.Pack configuration: https://re-pack.dev/docs/guides/configuration
+ */
+
+export default env => {
+  const {mode} = env;
+
+  return {
+    mode,
+    context: __dirname,
+    entry: {},
+    experiments: {
+      incremental: mode === 'development',
+    },
+    resolve: {
+      ...Repack.getResolveOptions(),
+    },
+    output: {
+      uniqueName: 'sas-news',
+    },
+    module: {
+      rules: [
+        ...Repack.getJsTransformRules(),
+        ...Repack.getAssetTransformRules({inline: true}),
+      ],
+    },
+    plugins: [
+      new Repack.RepackPlugin(),
+      new Repack.plugins.ModuleFederationPluginV2({
+        name: 'news',
+        filename: 'news.container.js.bundle',
+        dts: false,
+        exposes: {
+          './App': './src/navigation/MainNavigator'
+        },
+        shared: getSharedDependencies({eager: false}),
+      }),
+      new Repack.plugins.CodeSigningPlugin({
+        enabled: mode === 'production',
+        privateKeyPath: path.join('..', '..', 'code-signing.pem'),
+      }),
+      // silence missing @react-native-masked-view optionally required by @react-navigation/elements
+      new rspack.IgnorePlugin({
+        resourceRegExp: /^@react-native-masked-view/,
+      }),
+    ],
+  };
+};
